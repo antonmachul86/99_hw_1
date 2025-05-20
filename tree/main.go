b6c6670e4490e2fc7c78f162ee46da03ff0dc012
SuperGame <madahtoxa@mail.ru>
2025-05-20 18:09:26 +0300

    first com agan

---
+++ tree/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"os"
+)
+
+func dirTree(out, path, printFiles string) (string, string, string) {
+
+}
+func main() {
+	out := os.Stdout
+	if !(len(os.Args) == 2 || len(os.Args) == 3) {
+		panic("usage go run main.go . [-f]")
+	}
+	path := os.Args[1]
+	printFiles := len(os.Args) == 3 && os.Args[2] == "-f"
+	err := dirTree(out, path, printFiles)
+	if err != nil {
+		panic(err.Error())
+	}
+}
